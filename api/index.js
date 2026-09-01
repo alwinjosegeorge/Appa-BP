fd6085d863629dea907c5f01a3d29c3ecf536230
Alwin Jose George <alwinjosegeorge2028@cs.sjcetpalai.ac.in>
2026-09-01 23:47:00 +0530

    Add Vercel serverless configuration

---
+++ api/index.js
@@ -0,0 +1,8 @@
+import server from '../dist/server/server.js';
+
+export const GET = server.fetch;
+export const POST = server.fetch;
+export const PUT = server.fetch;
+export const DELETE = server.fetch;
+export const PATCH = server.fetch;
+export const OPTIONS = server.fetch;
